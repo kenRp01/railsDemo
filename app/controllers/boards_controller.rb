095ff3e7583b46c339724cb89e6f39d87cbc47a7
kensukeeee <yqxxnaxr1109@gmail.com>
2020-11-01 11:47:49 +0900

    パーシャルによる共通化

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,16 +3,27 @@ class BoardsController < ApplicationController
         @boards = Board.all
     end
 
-    def show
-        @board = Board.find(params[:id])
-    end
-
     def new
         @board = Board.new
     end
 
     def create
         Board.create(board_params)
+    end
+
+    def show
+        @board = Board.find(params[:id])
+    end
+
+    def edit
+        @board = Board.find(params[:id])
+    end
+
+    def update
+        board = Board.find(params[:id])
+        board.update(board_params)
+        #boards/:idにリダイレクト
+        redirect_to board
     end
 
     private
